6afd8dada176efc28d8e10836fb34965856eb559
Audrey Jiang <ajiang@lcs.on.ca>
2023-01-27 14:18:03 -0500

    Initial Commit

---
+++ LottieAnimationList/LottieAnimationListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationListApp.swift
+//  LottieAnimationList
+//
+//  Created by Audrey Jiang on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
